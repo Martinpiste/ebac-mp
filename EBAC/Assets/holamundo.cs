f6e72c151a7ecaa53feb434bc8d4ef0cb524c581
Martinpiste <martinpiste135@gmail.com>
2022-05-30 23:06:37 -0500

    Gameloop de Unity

--- EBAC/Assets/holamundo.cs
+++ EBAC/Assets/holamundo.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class holamundo : MonoBehaviour
 {
     // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("Hola desde awake");
+    }
+
     void Start()
     {
-        print("Algo sucedio");
-        Debug.Log("Algo esta paso");
-        Debug.LogWarning("Algo salio mediamente mal");
-        Debug.LogError("Algo salio mal");
+        //print("Algo sucedio");
+        //Debug.Log("Algo esta paso");
+        //Debug.LogWarning("Algo salio mediamente mal");
+        //Debug.LogError("Algo salio mal");
+        Debug.LogWarning("Hola desde Start");
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.LogError("Hola esto es el Update");
     }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Esto es le FixedUpdate");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde LateUpdate");
+    }
+
+
+
+
 }
